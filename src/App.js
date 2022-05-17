1dd4df3c1a3e5e3149c1a0b679ffeda72fc4207d
rohan876 <rejirohan5@gmail.com>
2022-05-17 12:09:35 +0530

    addbook

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import './App.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Addbooks from './components/Addbooks';
 
 function App() {
   return (<div>
-    <BrowserRouter>
-    <Routes>
-<Route/>
-
-    </Routes>
-    </BrowserRouter>
+    <Addbooks/>
     </div>
   );
 }
