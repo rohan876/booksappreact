174d91a0c486e16cf0bf1fcf4b3f8a7edbc8d979
rohan876 <rejirohan5@gmail.com>
2022-05-17 12:38:40 +0530

    search

--- src/components/Addbooks.js
+++ src/components/Addbooks.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Head from './Head'
 
 const Addbooks = () => {
   return (
     <div>
+        <Head/>
+        
 <div className="container">
         <div className="row g-3">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
